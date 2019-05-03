330bbd1df52bc13f4f48116450dcc45a5f9dfd50
Mysteriedingens <josefsle97@gmail.com>
2019-05-03 20:26:14 +0200

    Integration of Semantic-UI-React and CSS

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { Container, Header, Icon } from "semantic-ui-react";
 
 class App extends Component {
   render() {
+    const containerStyle = {
+      paddingTop: "50px"
+    };
     return (
-      <div>
+      <Container text style={containerStyle}>
+        <Header as="h2">
+          <Icon name="shopping cart" />
+          <Header.Content>Shopping List</Header.Content>
+        </Header>
         <h1> Hello </h1>
-      </div>
+      </Container>
     );
   }
 }
