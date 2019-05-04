3e0bb63be2cf6502240869332f0ed7b3a45bd271
Mysteriedingens <josefsle97@gmail.com>
2019-05-04 16:47:05 +0200

    Redux: initial State for items

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,22 @@
+import uuid from "uuid";
+
+const intialState = {
+  items: [
+    {
+      id: uuid(),
+      name: "Eggs"
+    },
+    {
+      id: uuid(),
+      name: "Meat"
+    },
+    {
+      id: uuid(),
+      name: "Milk"
+    },
+    {
+      id: uuid(),
+      name: "Steak"
+    }
+  ]
+};
