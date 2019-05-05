5be340353d6a86476f762bedec84f87fda2b394d
Mysteriedingens <josefsle97@gmail.com>
2019-05-05 19:48:56 +0200

    change dblink

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://josef:<josef97>@cluster0-ujpml.mongodb.net/test?retryWrites=true"
+    "mongodb+srv://josef:<josef97>@cluster0-ujpml.mongodb.net/mern_shoppinglist?retryWrites=true"
 };
 //Connecting to the backend
