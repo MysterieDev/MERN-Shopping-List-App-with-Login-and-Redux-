99b99e1969bd38a8c9e0e244e49287d49b678297
Mysteriedingens <josefsle97@gmail.com>
2019-05-05 19:57:13 +0200

    connecting to mongodbatlast

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://josef:<josef97>@cluster0-ujpml.mongodb.net/mern_shoppinglist?retryWrites=true"
+    "mongodb+srv://josef:josef97@cluster0-ujpml.mongodb.net/test?retryWrites=true"
 };
-//Connecting to the backend
