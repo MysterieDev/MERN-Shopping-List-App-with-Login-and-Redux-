d45c1ed33c8a7c5ac12c789e2a0d5c83cef7326e
Mysteriedingens <josefsle97@gmail.com>
2019-05-07 21:51:45 +0200

    Setting up the route for Users-API

--- server.js
+++ server.js
@@ -1,26 +1,24 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const path = require("path");
-const items = require("./routes/api/items");
 
 const app = express();
 
 //BodyParser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
 const db = require("./config/keys").mongoURI;
 
 //Connect to Mongo
 mongoose
-  .connect(db, { useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
   .then(() => console.log("Mongo DB connected"))
   .catch(err => console.log(err));
 
 //Use Routes
-app.use("/api/items", items);
-
+app.use("/api/items", require("./routes/api/items"));
+app.use("/api/users", require("./routes/api/users"));
 //Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
