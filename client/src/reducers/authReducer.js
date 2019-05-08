bbc1d1df7d40fd03002275d0404b8f05ac296fdb
Mysteriedingens <josefsle97@gmail.com>
2019-05-08 20:42:06 +0200

    Login: Error Reducer & Auth Reducer

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,24 +1,54 @@
-import { GET_ERRORS, CLEAR_ERRORS } from "../actions/types";
+import {
+  USER_LOADED,
+  USER_LOADING,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT_SUCCESS,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL
+} from "../actions/types";
 
 const initialState = {
-  msg: {},
-  status: null,
-  id: null
+  token: localStorage.getItem("token"),
+  isAuthenticated: null,
+  isLoading: false,
+  user: null
 };
 
 export default function(state = initialState, action) {
   switch (action.type) {
-    case GET_ERRORS:
+    case USER_LOADING:
       return {
-        msg: action.payload.msg,
-        status: action.payload.status,
-        id: action.payload.id
+        ...state,
+        isLoading: true
       };
-    case CLEAR_ERRORS:
+    case USER_LOADED:
       return {
-        msg: {},
-        status: null,
-        id: null
+        ...state,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload
+      };
+    case LOGIN_SUCCESS:
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        isLoading: false,
+        user: action.payload
+      };
+    case AUTH_ERROR:
+    case LOGIN_FAIL:
+    case LOGOUT_SUCCESS:
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        token: null,
+        user: null,
+        isAuthenticated: false,
+        isLoading: false
       };
     default:
       return state;
